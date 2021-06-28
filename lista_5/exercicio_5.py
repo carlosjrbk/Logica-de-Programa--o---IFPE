46fe89de4b7a913bc65c5e2cabd3bd7b2b823735
carlosjr <83727890+carlosjrbk@users.noreply.github.com>
2021-06-28 01:03:00 -0300

    exercicio 5 inserido

--- lista_5/exercicio_5.py
+++ lista_5/exercicio_5.py
@@ -1,1 +1,14 @@
-          
+ms = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+temp = []
+
+for m in range(0, len(ms)):
+    temp.append(float(input("Digite a temperatura do mes de " + ms[m] + ": ")))
+
+mAnual = sum(temp)/len(temp)
+print('-'*45)
+print('Meses Com a Temperatura acima da media')
+print("-"*45)
+for m in range(0, len(temp)):
+    if temp[m] > mAnual:
+        
+        print (str(m+1) + " - " + ms[m])         
